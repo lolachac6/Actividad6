f9a8263ebe70abd2815a3bd95a78e293f1069d32
Lola <lola_chacon8@hotmail.com>
2024-08-05 19:15:51 +0200

    Inyeccion de dependencias + inyeccion de routerLink + ngOnInit

--- src/app/pages/user-view/user-view.component.ts
+++ src/app/pages/user-view/user-view.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { UsersService } from '../../services/users.service';
+import { IResult } from '../../interfaces/iresult.interfaces';
+
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
+  activeRoute = inject(ActivatedRoute)
+  serviceUser= inject(UsersService)
+  singleUser!:IResult
+  
+  ngOnInit(){
+    this.activeRoute.params.subscribe( async params => {
+      let id = (params['id'])
+     this.singleUser = await this.serviceUser.getById(id)
+     
+ 
+    })
+  }
 
 }
